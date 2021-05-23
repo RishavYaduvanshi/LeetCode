de6701bf26515d552be11f4f4ec6446f95605634
Rishav <67699808+RishavYaduvanshi@users.noreply.github.com>
2021-05-23 07:49:20 +0000

    Sync LeetCode submission - Find the Duplicate Number (cpp)

---
+++ LT_Folder/problems/find_the_duplicate_number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        for(auto x:mp)
+            if(x.second>1)
+                ans=x.first;
+        return ans;
+    }
+};
