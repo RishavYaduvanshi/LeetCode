03cc3f41019c8ba5997a646184d640ddc2bbf903
Rishav <67699808+RishavYaduvanshi@users.noreply.github.com>
2022-03-15 09:24:14 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- LT_Folder/problems/palindrome_number/solution.cpp
+++ LT_Folder/problems/palindrome_number/solution.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if(x < 0)
-            return false;
-        string t = to_string(x);
-        string temp = t;
-        reverse(t.begin(),t.end());
-        if(t == temp)
+    
+    bool solve(int n)
+    {
+        int t = n;
+        long long sum = 0;
+        while (n)
+        {
+            int r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
+        }
+        if (t == sum)
             return true;
         else
             return false;
+    }
+    bool isPalindrome(int x) {
         
+        if(x < 0)
+            return false;
+        return solve(x);
     }
 };
