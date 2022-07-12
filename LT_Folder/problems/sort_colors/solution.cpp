84316e01eb9d39cc150510a50e2c5e5de3a9a67a
Rishav <67699808+RishavYaduvanshi@users.noreply.github.com>
2022-07-12 16:32:39 +0000

    Sync LeetCode submission - Sort Colors (cpp)

--- LT_Folder/problems/sort_colors/solution.cpp
+++ LT_Folder/problems/sort_colors/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
+    void sortColors(vector<int>& nums) {
         
-        int l=0,m=0,r=a.size()-1;
-        
-        while(m<=r)
-        {
-            if(a[m]==0)
-            {
-                swap(a[m],a[l]);
-                m++;
-                l++;
+        int left_pointer = 0;
+        int middile_pointer = 0;
+        int right_pointer = nums.size() - 1;
+        while(middile_pointer <= right_pointer){
+            if(nums[middile_pointer] == 0){
+                swap(nums[middile_pointer],nums[left_pointer]);
+                middile_pointer++;
+                left_pointer++;
             }
-            else if(a[m]==1)
-                m++;
-            else if(a[m]==2)
-            {
-                swap(a[m],a[r]);
-                r--;
+            else if(nums[middile_pointer] == 1){
+                middile_pointer++;
             }
-            
+            else if(nums[middile_pointer] == 2){
+                swap(nums[middile_pointer],nums[right_pointer]);
+                right_pointer--;
+            }
         }
+        
         
     }
 };
