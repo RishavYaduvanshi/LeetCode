881e99a38f093666d78c7c125b99507f236653cd
Rishav <67699808+RishavYaduvanshi@users.noreply.github.com>
2022-07-15 12:18:24 +0000

    Sync LeetCode submission - Max Number of K-Sum Pairs (cpp)

---
+++ LT_Folder/problems/max_number_of_k-sum_pairs/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> mp;
+        
+        int ans = 0;
+        
+        for(int i : nums){
+            if(mp.find(k - i) != mp.end() and mp[k - i] > 0){
+                ans ++;
+                mp[k - i]--;
+            }
+            else
+                mp[i]++;
+        }
+        return ans;
+    }
+};
