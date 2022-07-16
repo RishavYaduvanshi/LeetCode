d23ca3760978a2ad15cf879f74b46ffd1a3a43d2
Rishav <67699808+RishavYaduvanshi@users.noreply.github.com>
2022-07-16 19:44:27 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- LT_Folder/problems/longest_consecutive_sequence/solution.cpp
+++ LT_Folder/problems/longest_consecutive_sequence/solution.cpp
@@ -1,39 +1,20 @@
 class Solution {
-    
-    
 public:
     int longestConsecutive(vector<int>& nums) {
         
-        map<int,int>mp;
-        
-        for(int i:nums)
+        map<int,int> mp;
+        for(int i : nums)
             mp[i]++;
-        
-        int ans=0;
-        int fin_ans=0;
-        int mn=INT_MAX;
-        for(auto x:mp)
-        {
-            if(mp.find(x.first+1)!=mp.end())
-                ans++;
-            else
-            {
-                fin_ans=max(fin_ans,ans+1);
-                ans=0;
+        int cnt = 0;
+        int ans = 0;
+        for(auto i : mp){
+            if(mp.find(i.first + 1) != mp.end())
+                cnt++;
+            else{
+                ans = max(ans, cnt + 1);
+                cnt = 0;
             }
         }
-        
-        
-        // for(int i=mn;i<=1000000000;i++)
-        // {
-        //     if(mp.find(i)==mp.end())
-        //         break;
-        //     ans++;
-        // }
-        return fin_ans;
-            
-        
-        
-        
+        return ans;
     }
 };
