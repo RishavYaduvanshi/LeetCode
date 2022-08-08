373ff536ed79998bddffb3e644f8595bce7906fa
Rishav <67699808+RishavYaduvanshi@users.noreply.github.com>
2022-08-08 07:22:46 +0000

    Sync LeetCode submission - Subsets (cpp)

--- LT_Folder/problems/subsets/solution.cpp
+++ LT_Folder/problems/subsets/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
     
 private:
-    void solveFun(vector<int> inp, vector<int> op, vector<vector<int>> &ans){
+    void solveFun(vector<int> inp, int ind,vector<int> ds, vector<vector<int>> &ans){
         
-        if(inp.size() == 0){
-            ans.push_back(op);
+        if(ind == inp.size()){
+            ans.push_back(ds);
             return;
         }
-        vector<int> op1 = op;
-        vector<int> op2 = op;
-        op2.push_back(inp[0]);
-        inp.erase(inp.begin()+0);
-        solveFun(inp,op1,ans);
-        solveFun(inp,op2,ans);
+        ds.push_back(inp[ind]);
+        solveFun(inp,ind+1,ds,ans);
+        ds.pop_back();
+        solveFun(inp,ind+1,ds,ans);
         
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
        
         vector<vector<int>> ans;
-        vector<int> op;
-        solveFun(nums,op,ans);
+        vector<int> ds;
+        solveFun(nums,0,ds,ans);
         return ans;
     }
 };
